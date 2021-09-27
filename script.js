004210dbc188ae66ed27eb52b29b70e92845ba5c
Felipe Mohr <felipe18mohr@gmail.com>
2021-09-27 01:47:51 -0300

    Add parallax effect. Need to improve images

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
     $(window).scroll(function(){
         // sticky navbar on scroll script
-        if(this.scrollY > 20){
+        if(this.scrollY > 400){
             $('.navbar').addClass("sticky");
         }else{
             $('.navbar').removeClass("sticky");
@@ -71,3 +71,23 @@ $(document).ready(function(){
         }
     });
 });
+
+let stars = document.getElementById('stars');
+let rock = document.getElementById('rock');
+let fire = document.getElementById('fire');
+let robots = document.getElementById('robots');
+let contact_btn = document.getElementById('contact_btn');
+
+window.addEventListener('scroll', function()
+{
+    let value = window.scrollY;
+    stars.style.left = -0.4 * value + 'px';
+    if (value <= 175)
+    {
+        robots.style.marginTop = 0.5 * value + 'px';
+        fire.style.top = 0.2 * value + 'px';
+    }
+
+    contact_btn.style.opacity = 100 - 0.3*value + '%';
+
+})
